081876349e93f164c7a2e115589ca18e881bccee
xyzw12345 <wangjt2020@163.com>
2025-04-04 16:52:18 +0800

    merge remote

--- CohenMacaulay/Dependency/AssociatedPrime.lean
+++ CohenMacaulay/Dependency/AssociatedPrime.lean
@@ -5,14 +5,45 @@ import «CohenMacaulay».Dependency.StableSES
 # Missing lemmas in Mathlib of Associated Prime
 -/
 
-variable {R M : Type*} [CommRing R] [IsNoetherianRing R] [AddCommGroup M] [Module R M]
+universe u v
+variable (R : Type u) (M : Type v) [CommRing R] [AddCommGroup M] [Module R M] [Module.Finite R M]
 
-theorem exists_LESeries_quotient_cyclic :
+theorem exists_LTSeries_quotient_cyclic:
     ∃ (p : LTSeries (Submodule R M)), p.head = ⊥ ∧ p.last = ⊤ ∧
     ∀ (i : Fin p.length), ∃ P : Ideal R, Nonempty (
-    ((p i.succ) ⧸ (Submodule.comap (p i.succ).subtype (p (Fin.castSucc i)))) ≃ₗ[R] (R ⧸ P)) := sorry
+    ((p i.succ) ⧸ (Submodule.comap (p i.succ).subtype (p (Fin.castSucc i)))) ≃ₗ[R] (R ⧸ P)) := by
+  generalize h : (⊤ : Submodule R M).spanFinrank = n
+  induction n generalizing M
+  case zero =>
+    have : (⊤ : Submodule R M).spanRank = 0 := by sorry
+    refine ⟨⟨0, fun _ ↦ ⊥, Fin.elim0⟩, ⟨rfl, Eq.symm (show ⊤ = ⊥ from ?_), ?_⟩⟩
+    · simpa using this
+    · simp
+  case succ n ih _ _ _ =>
+    obtain ⟨s, hs, x, hxs⟩ : ∃ (S : {S : Set M | S.Finite}), S.2.toFinset.card = n ∧
+      ∃ (x : M), (Submodule.span R (Set.insert x S.1) = ⊤) := sorry
+    have h1 : (⊤ : Submodule R (Submodule.span R s.1)).spanFinrank = n := sorry
+    have h2 : Module.Finite R (Submodule.span R s.1) := sorry
+    have h3 : Submodule.span R s.1 < ⊤ := sorry
+    obtain ⟨p', h_head', h_tail', hp'⟩ := @ih (Submodule.span R s.1) _ _ h2 h1
+    use RelSeries.snoc (LTSeries.map p' (Submodule.map (Submodule.subtype (Submodule.span R s.1)))
+      (Submodule.map_strictMono_of_injective <| Submodule.subtype_injective _)) ⊤ ?_
+    swap
+    · simpa [h_tail']
+    · simp only [Set.mem_setOf_eq, RelSeries.head_snoc, LTSeries.head_map, h_head',
+      Submodule.map_bot, RelSeries.last_snoc, LTSeries.last_map, id_eq, eq_mpr_eq_cast,
+      RelSeries.snoc_length, LTSeries.map_length, true_and]
+      intro i
 
-theorem exists_LESeries_quotient_iso_quotient_prime :
+      sorry
+
+
+
+
+
+variable [IsNoetherianRing R]
+
+theorem exists_LTSeries_quotient_iso_quotient_prime :
     ∃ (p : LTSeries (Submodule R M)), p.head = ⊥ ∧ p.last = ⊤ ∧
     ∀ (i : Fin p.length), ∃ P : Ideal R, P.IsPrime ∧ Nonempty (
     ((p i.succ) ⧸ (Submodule.comap (p i.succ).subtype (p (Fin.castSucc i)))) ≃ₗ[R] (R ⧸ P)) := sorry
--- CohenMacaulay/Dependency/StableSES.lean
+++ CohenMacaulay/Dependency/StableSES.lean
@@ -1,19 +1,22 @@
 import Mathlib
 
-open CategoryTheory
-variable {C : Type*} [Category C] [Limits.HasZeroMorphisms C]
+-- open CategoryTheory
+-- variable {C : Type*} [Category C] [Limits.HasZeroMorphisms C]
 
-class StableSES (P : C -> Prop) : Prop where
-  of_isZero : (X : C) → Limits.IsZero X → P X
-  stability : (S : ShortComplex C) -> S.Exact -> P (S.X₁) -> P (S.X₃) -> P (S.X₂)
+-- class StableSES (P : C -> Prop) : Prop where
+--   of_isZero : (X : C) → Limits.IsZero X → P X
+--   stability : (S : ShortComplex C) -> S.ShortExact -> P (S.X₁) -> P (S.X₃) -> P (S.X₂)
 
-theorem iso_trans_of_StableSES [Limits.HasZeroObject C] (P : C → Prop) [StableSES P] {X Y : C} (h : Iso X Y) : P X ↔ P Y := sorry
+-- theorem iso_trans_of_StableSES [Limits.HasZeroObject C] (P : C → Prop) [StableSES P] {X Y : C} (h : Iso X Y) : P X ↔ P Y := sorry
 
 section ModuleCat
 
 variable {R : Type*} [Ring R]
 
-theorem fg_induction (P : ModuleCat R → Prop) [StableSES P] (h : ∀ I : Ideal R, P (ModuleCat.of R (R ⧸ I)))
+theorem fg_induction (P : ModuleCat R → Prop)
+    (h_zero : ∀ (N : ModuleCat R), Subsingleton N → P N)
+    (h_base : ∀ (N : ModuleCat R), (⊤ : Submodule R N).IsPrincipal → P N)
+    (h_ext : ∀ (M : ModuleCat R), ∀ (N : Submodule R M), P (ModuleCat.of R N) → P (ModuleCat.of R (M ⧸ N)) → P M)
     (M : ModuleCat R) (hM : Module.Finite R M) : P M := by
   sorry
 
