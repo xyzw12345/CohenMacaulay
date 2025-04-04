450f2537cc2e8fa68e166476f3d8fbb947d5d501
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-04-04 14:12:25 +0800

    proof 212a

--- CohenMacaulay/lemma212.lean
+++ CohenMacaulay/lemma212.lean
@@ -1,3 +1,10 @@
-import Mathlib
+import Mathlib.RingTheory.Regular.IsSMulRegular
 
-variable {R : Type*} [CommRing R]
+variable {R M N : Type*} [CommRing R] [AddCommGroup M] [AddCommGroup N] [Module R M] [Module R N]
+
+lemma lemma_212_a {r : R} (reg : IsSMulRegular M r)
+    (mem_ann : r ∈ (⊤ : Submodule R N).annihilator) : Subsingleton (N →ₗ[R] M) := by
+  apply subsingleton_of_forall_eq 0 (fun f ↦ LinearMap.ext fun x ↦ ?_)
+  have : r • (f x) = r • 0 := by
+    rw [smul_zero, ← map_smul, Submodule.mem_annihilator.mp mem_ann x trivial, map_zero]
+  simpa using reg this
