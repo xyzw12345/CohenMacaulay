b7b53064a558db8784443d7fd49d028a82442667
xyzw12345 <wangjt2020@163.com>
2025-04-04 14:35:37 +0800

    add lemmas

--- Dependency/AssociatedPrime.lean
+++ Dependency/AssociatedPrime.lean
@@ -1,10 +1,25 @@
-import Mathlib.RingTheory.Ideal.AssociatedPrime
+import Mathlib
 
 /-!
 # Missing lemmas in Mathlib of Associated Prime
 -/
 
-variable {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M]
+variable {R M : Type*} [CommRing R] [IsNoetherianRing R] [AddCommGroup M] [Module R M]
 
-theorem AssociatedPrimes.finite_of_noetherian [IsNoetherianRing R] :
-    (associatedPrimes R M).Finite := sorry
+theorem exists_LESeries_quotient_cyclic :
+    ∃ (p : LTSeries (Submodule R M)), p.head = ⊥ ∧ p.last = ⊤ ∧
+    ∀ (i : Fin p.length), ∃ P : Ideal R, Nonempty (
+    ((p i.succ) ⧸ (Submodule.comap (p i.succ).subtype (p (Fin.castSucc i)))) ≃ₗ[R] (R ⧸ P)) := sorry
+
+theorem exists_LESeries_quotient_iso_quotient_prime :
+    ∃ (p : LTSeries (Submodule R M)), p.head = ⊥ ∧ p.last = ⊤ ∧
+    ∀ (i : Fin p.length), ∃ P : Ideal R, P.IsPrime ∧ Nonempty (
+    ((p i.succ) ⧸ (Submodule.comap (p i.succ).subtype (p (Fin.castSucc i)))) ≃ₗ[R] (R ⧸ P)) := sorry
+
+theorem AssociatedPrimes.of_quotient_iso_quotient_prime (p : LTSeries (Submodule R M)) (h_head : p.head = ⊥)
+    (h_last : p.last = ⊤) (P : Fin p.length → Ideal R) (hP : ∀ (i : Fin p.length), (P i).IsPrime ∧
+    Nonempty (((p i.succ) ⧸ (Submodule.comap (p i.succ).subtype (p (Fin.castSucc i)))) ≃ₗ[R] (R ⧸ (P i)))) :
+    (associatedPrimes R M) ⊆ P '' Set.univ := sorry
+
+theorem AssociatedPrimes.finite_of_noetherian [IsNoetherianRing R] : (associatedPrimes R M).Finite := by
+  sorry
