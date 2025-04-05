f15a0db5d76ca2e6842739c511e99701c0c10201
Yongle Hu <140475041+mbkybky@users.noreply.github.com>
2025-04-05 10:54:02 +0800

    Update lemma213.lean

--- CohenMacaulay/lemma213.lean
+++ CohenMacaulay/lemma213.lean
@@ -7,13 +7,13 @@ universe u v w
 open RingTheory.Sequence Ideal CategoryTheory
 set_option pp.universes true
 variable {R : Type u} [CommRing R] {M N : ModuleCat.{v} R} {n : ℕ}
-  [LocallySmall.{w} (ModuleCat.{max u v} R)]
+  [UnivLE.{max u v, w}]
+  --[LocallySmall.{w} (ModuleCat.{max u v} R)]
   {rs : List R} (hr : IsWeaklyRegular M rs) (h : ∀ r : R, r ∈ rs → r ∈ Module.annihilator R N)
 
-instance : EnoughProjectives (ModuleCat.{max u v} R) := inferInstance
-
-local instance : CategoryTheory.HasExt.{w} (ModuleCat.{max u v} R) :=
-  CategoryTheory.hasExt_of_enoughProjectives.{w} (ModuleCat.{max u v} R)
+local instance : CategoryTheory.HasExt.{max u v + 1} (ModuleCat.{max u v} R) :=
+  CategoryTheory.HasExt.standard (ModuleCat.{max u v} R)
+  --CategoryTheory.hasExt_of_enoughProjectives.{w} (ModuleCat.{max u v} R)
 
 /- def lemma_213 : (N →ₗ[R] M ⧸ (ofList rs • ⊤ : Submodule R M)) ≃ₗ[R] Ext R _ n M N := by
   sorry -/
