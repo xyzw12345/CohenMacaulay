34ca8de6d07c9c506f8a6e8ff03edbcbcdedca52
xyzw12345 <wangjt2020@163.com>
2025-04-06 23:21:43 +0800

    refactor lemma

--- CohenMacaulay/Dependency/SMulRegular.lean
+++ CohenMacaulay/Dependency/SMulRegular.lean
@@ -1,13 +1,4 @@
-import Mathlib.Algebra.Algebra.Operations
-import Mathlib.Algebra.Category.ModuleCat.Basic
-import Mathlib.Algebra.Homology.ShortComplex.Basic
-import Mathlib.LinearAlgebra.Quotient.Defs
-import Mathlib.RingTheory.Ideal.Span
-import Mathlib.Algebra.Homology.ShortComplex.ShortExact
-import Mathlib.Algebra.Category.ModuleCat.EpiMono
-import Mathlib.Algebra.Homology.ShortComplex.ModuleCat
-import Mathlib.RingTheory.Ideal.Operations
-import Mathlib.Algebra.Module.Submodule.Pointwise
+import Mathlib
 
 universe u v w
 
@@ -19,15 +10,15 @@ def SMul_ShortComplex (r : R) :
     ShortComplex (ModuleCat R) where
   X₁ := M
   X₂ := M
-  X₃ := ModuleCat.of R (M ⧸ (span {r} • (⊤ : Submodule R M) : Submodule R M))
+  X₃ := ModuleCat.of R (QuotSMulTop r M)
   f := ModuleCat.ofHom (r • LinearMap.id)
-  g := ModuleCat.ofHom ((span {r} • (⊤ : Submodule R M)).mkQ)
+  g := ModuleCat.ofHom (r • (⊤ : Submodule R M)).mkQ
   zero := by
     ext m
     simp only [ModuleCat.hom_comp, ModuleCat.hom_ofHom, LinearMap.coe_comp, Function.comp_apply,
       LinearMap.smul_apply, LinearMap.id_coe, id_eq, Submodule.mkQ_apply,
       ModuleCat.hom_zero, LinearMap.zero_apply, Submodule.Quotient.mk_eq_zero]
-    refine Submodule.smul_mem_smul (mem_span_singleton_self r) trivial
+    exact Submodule.smul_mem_pointwise_smul m r ⊤ trivial
 
 variable {M} in
 lemma IsSMulRegular.SMul_ShortComplex_exact {r : R} (reg : IsSMulRegular M r) :
