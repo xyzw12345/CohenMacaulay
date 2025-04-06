7583d178656fe19d8340748aeaebd3e07de0a34b
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-04-06 23:00:29 +0800

    Update lemma222.lean

--- CohenMacaulay/lemma222.lean
+++ CohenMacaulay/lemma222.lean
@@ -16,34 +16,42 @@ local instance : CategoryTheory.HasExt.{w} (ModuleCat.{max u v} R) :=
 
 set_option linter.unusedTactic false
 lemma lemma222_3_to_4 (I : Ideal R) (n : ℕ) : ∀ M : ModuleCat R, Nontrivial M → Module.Finite R M →
+    I • (⊤ : Submodule R M) < ⊤ →
     (∃ N : ModuleCat R, Nontrivial N ∧ Module.Finite R N ∧ Module.support R N = PrimeSpectrum.zeroLocus I ∧
     ∀ i < n, Subsingleton (Ext N M i)) → ∃ rs : List R, rs.length = n ∧
     (∀ r ∈ rs, r ∈ I) ∧ RingTheory.Sequence.IsRegular M rs := by
   induction' n with n ih
-  · intro M ntr M_fin exist_N
+  · intro M ntr M_fin smul_lt exist_N
     use []
     simp [isRegular_iff]
-  · intro M ntr M_fin exist_N
+  · intro M ntr M_fin smul_lt exist_N
     rcases exist_N with ⟨N, ntr, fin, h_supp, h_ext⟩
+    have h_supp' := h_supp
+    rw [Module.support_eq_zeroLocus, PrimeSpectrum.zeroLocus_eq_iff] at h_supp'
     have : Subsingleton (N →ₗ[R] M) :=
       let _ := h_ext 0 n.zero_lt_succ
       let _ : Subsingleton (N ⟶ M) :=
         Equiv.subsingleton.symm (homEquiv₀_hom N M).toEquiv
       Equiv.subsingleton.symm (ModuleCat.homAddEquiv (M := N) (N := M)).toEquiv
     rcases lemma_212_b this with ⟨x, mem_ann, hx⟩
-    have ntr' : Nontrivial (M ⧸ span {x} • (⊤ : Submodule R M)) := by
+    let M' := M ⧸ span {x} • (⊤ : Submodule R M)
+    have ntr' : Nontrivial M' := by
       apply Submodule.Quotient.nontrivial_of_lt_top
-
+      rw [Submodule.ideal_span_singleton_smul]
+      have := Ideal.le_radical mem_ann
+      rw [h_supp', Ideal.mem_radical_iff] at this
+      rcases this with ⟨k, hk⟩
+      have := IsSMulRegular.pow k hx
       sorry
-    have fin' : Module.Finite R (M ⧸ span {x} • (⊤ : Submodule R M)) :=
-      Module.Finite.quotient R (span {x} • ⊤)
+    have smul_lt : I • (⊤ : Submodule R M') < ⊤ := by
+      sorry
     let seq := Ext.covariantSequence N hx.SMul_ShortComplex_exact
     let seq_exact := Ext.covariantSequence_exact N hx.SMul_ShortComplex_exact
     #check CategoryTheory.ComposableArrows.sc
 
     have exist_N' : (∃ N : ModuleCat R, Nontrivial ↑N ∧ Module.Finite R ↑N ∧
         Module.support R ↑N = PrimeSpectrum.zeroLocus ↑I ∧
-          ∀ i < n, Subsingleton (Abelian.Ext N (ModuleCat.of R (M ⧸ span {x} • (⊤ : Submodule R M))) i)) := by
+          ∀ i < n, Subsingleton (Abelian.Ext N (ModuleCat.of R M') i)) := by
       use N
       simp only [ntr, fin, h_supp, true_and]
       intro i hi
@@ -51,7 +59,7 @@ lemma lemma222_3_to_4 (I : Ideal R) (n : ℕ) : ∀ M : ModuleCat R, Nontrivial 
       sorry
 
     rcases ih (ModuleCat.of R (M ⧸ span {x} • (⊤ : Submodule R M))) ntr'
-      (Module.Finite.quotient R _) exist_N' with ⟨rs, len, mem, reg⟩
+      (Module.Finite.quotient R _) smul_lt exist_N' with ⟨rs, len, mem, reg⟩
     -- x is only in the radical of I
 
     sorry
