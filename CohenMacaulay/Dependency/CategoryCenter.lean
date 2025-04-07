aac4464c82242591ee11471ec5199ce711920cc7
Ruiqi Chen <ruiqi_chen@stu.pku.edu.cn>
2025-04-07 10:21:24 +0800

    Update center

--- CohenMacaulay/Dependency/CategoryCenter.lean
+++ CohenMacaulay/Dependency/CategoryCenter.lean
@@ -2,14 +2,33 @@ import Mathlib
 
 namespace CategoryTheory
 
+section CenterZ
 universe u v
 variable (C : Type v) [Category.{u,v} C]
 
-def CategoryTheory.CenterZ : Type max u v := End (𝟭 C)
+def CenterZ : Type max u v := End (𝟭 C)
 
-instance CategoryTheory.CenterZ.monoid : Monoid (CategoryTheory.CenterZ C) := CategoryTheory.End.monoid
+instance CenterZ.monoid : Monoid (CenterZ C) := CategoryTheory.End.monoid
+instance CenterZ.comm_monoid : CommMonoid (CenterZ C) where
+  mul_comm := fun a b => NatTrans.id_comm b a
 
-def CategoryTheory.CenterZ.ring_action : sorry := sorry
+instance CenterZ.comm_ring [Preadditive C] : CommRing (CenterZ C) := sorry
 
-def CategoryTheory.CenterZ.complex_map : sorry := sorry
-def CategoryTheory.CenterZ.localization_map : sorry := sorry
+end CenterZ
+
+def CenterZ.ring_action (R : Type*) [CommRing R] : R →+* CenterZ (ModuleCat R) where
+  toFun := fun r => {
+    app := fun M => ModuleCat.ofHom (r • LinearMap.id)
+    naturality := by -- aesop proof
+      intro X Y f
+      simp_all only [Functor.id_obj, Functor.id_map]
+      ext x : 2
+      simp_all only [ModuleCat.hom_comp, ModuleCat.hom_ofHom, LinearMap.coe_comp, Function.comp_apply,
+        LinearMap.smul_apply, LinearMap.id_coe, id_eq, map_smul]
+  }
+  map_one' := sorry
+  map_mul' := sorry
+  map_zero' := sorry
+  map_add' := sorry
+def CenterZ.complex_map (A : Type*) : sorry := sorry
+def CenterZ.localization_map : sorry := sorry
